4867893b168fb4fa2ba7ea55d8a8b20f921e53ec
Nguyen Huy Hoang <nguyenhuyhoang31102005@gmail.com>
2026-07-14 21:53:48 +0700

    admin-repo

---
+++ Backend/src/main/java/backend/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package backend.repository;
+
+import backend.model.User;
+import backend.model.enums.UserStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Page<User> findByFullNameContainingIgnoreCaseOrEmailContainingIgnoreCase(
+            String fullName,
+            String email,
+            Pageable pageable
+    );
+    Page<User> findByStatus(UserStatus status, Pageable pageable);
+
+}
